cb9be6cd4f0b544f650f30082e4faf7d436a303f
reutby <reutby321@gmail.com>
2021-01-16 19:29:31 +0200

    adding active design to the navigation bar when scroling to each section

--- index.js
+++ index.js
@@ -17,11 +17,12 @@ $(function () {
   // NAV POSITION
   var navPos = $('.navigation').position().top;
   var lastPos = 0;
+
   var isMobile = false;
 
   $(window).on('scroll', function () {
     var pos = $(window).scrollTop();
-
+    var pos2 = pos + 50;
     if (!isMobile) {
       if (pos >= navPos + $('.navigation').height() && lastPos < pos) {
         $('.navigation').addClass('fixed-position navigation-animation');
@@ -32,30 +33,63 @@ $(function () {
       lastPos = pos;
     }
 
+
+    // Link Highlighting
+    if (pos2 > $('#section-home').offset().top) {
+      highlightLink('home');
+    }
+    if (pos2 > $('#section-about').offset().top) {
+      highlightLink('about');
+    }
+    if (pos2 > $('#section-skills').offset().top) {
+      highlightLink('skills');
+    }
+    if (pos2 > $('#section-projects').offset().top) {
+      highlightLink('projects');
+    }
+    if (pos2 > $('#section-contact-me').offset().top) {
+      highlightLink('contact');
+    }
+
+
+    //   lockTimer = setTimeout(function() {
+    //     $('body').removeClass('disable-hover');
+    //   }, 500);
+    // });
+
   });
-  
+  function highlightLink(anchor) {
+    $('ul .active').removeClass('active');
+    $('ul')
+      .find('[dest="' + anchor + '"]')
+      .children().addClass('active');
+  }
+
+
+
   //CONTACT FORM
-  $("#contact-form").on('submit',(e)=> {
+  $("#contact-form").on('submit', (e) => {
     e.preventDefault();
 
     $.ajax({
       url: 'https://formspree.io/f/xknpkazq',
       method: 'POST',
-      data: { message:($('form').serialize()) },
+      data: { message: ($('form').serialize()) },
       dataType: 'json'
-    }).done(function(res) {
+    }).done(function (res) {
       $('#success-message').addClass('message-visible');
-      console.log(res);
+
       $('#contact-form')
         .find('input[type=text], input[type=email], textarea')
         .val('');
-    }).fail((err)=>{console.log(err)
+    }).fail((err) => {
+
       $('#failed-message').addClass('message-visible');
-    
+
     });
   });
 
-  
+
   function is_in_view($element, element_class, element_bottom_position, window_top_position, element_top_position, window_bottom_position) {
     if ((element_bottom_position >= window_top_position) &&
       (element_top_position <= window_bottom_position)) {
@@ -68,13 +102,13 @@ $(function () {
     var window_height = $window.height();
     var window_top_position = $window.scrollTop();
     var window_bottom_position = (window_top_position + window_height);
-  
+
     $.each($element_target, function () {
       var $element = $(this);
       var element_height = $element.outerHeight();
       var element_top_position = $element.offset().top;
       var element_bottom_position = (element_top_position + element_height);
-  
+
       //check to see if this current container is within viewport
       callback($element, element_class, element_bottom_position, window_top_position, element_top_position, window_bottom_position);
     });
